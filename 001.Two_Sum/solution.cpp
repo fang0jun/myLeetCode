f9ce4df494261b31a2b01a1601a642db74dbfc71
fang0jun <fang0jun@qq.com>
2020-09-17 18:51:10 +0800

    更新题目001

---
+++ 001.Two_Sum/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hash;
+        for(int i = 0; i < nums.size(); i++){
+            hash[nums[i]] = i;
+        }
+
+        for(int i = 0; i < nums.size(); i++){
+            if(hash.find(target-nums[i]) != hash.end() && hash[target - nums[i]] != i) return {i, hash[target - nums[i]]};
+        }
+        return {};
+    }
+};
